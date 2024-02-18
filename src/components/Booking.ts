40d4ae544df1c34b11c44011957f606aecfb36c2
Fabian0207 <fabianolsson28@gmail.com>
2024-02-18 20:52:55 +0100

    fix

--- src/components/Booking.ts
+++ src/components/Booking.ts
@@ -2,7 +2,7 @@
 import { Customer } from "./Customer";
 
 export class Bookings{
-    constructor(public restaurantId:string, public date:string, public time:string, public numberOfGuests:number, public customer: User){
+    constructor(public restaurantId:string, public date:string, public time:string, public numberOfGuests:number, public customer: Customer){
 
     }
 }
