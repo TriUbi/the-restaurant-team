9481b7800807230a58c0c2d1fc35e5644cda4642
PatrikBajkusa <patrikb99@hotmail.com>
2024-02-19 12:31:29 +0100

    Added tons of admin stuff

--- src/pages/Admin.tsx
+++ src/pages/Admin.tsx
@@ -1,12 +1,47 @@
-import { ChangeEvent, useState } from "react";
+import { ChangeEvent, useEffect, useState } from "react";
 import "./../styles/Admin.scss";
+import { IBooking } from "../components/IBooking";
+import axios from "axios";
+import { Bookings } from "../components/Booking";
 
 export const Admin = () => {
+  const restaurantID = "65cdf38894d2af1c6aeae91d";
+  const [bookings, setBookings] = useState<IBooking[]>([]);
   const [showAdmin, setShowAdmin] = useState(false);
   const [showButton, setShowButton] = useState(true);
   const [showLogIn, setShowLogIn] = useState(false);
   const [userName, setUserName] = useState("");
   const [password, setPassword] = useState("");
+  const [showBookingDone, setShowBookingDone] = useState(true);
+
+  const [showChange, setShowChange] = useState(false);
+  const [bookingToUpdate, setBookingToUpdate] = useState<IBooking>({
+    _id: "",
+    restaurantId: restaurantID,
+    date: "",
+    time: "",
+    numberOfGuests: 0,
+    customerId: "",
+  });
+  const [updatedBooking, setUpdatedBooking] = useState<IBooking>({
+    _id: "",
+    restaurantId: restaurantID,
+    date: "",
+    time: "",
+    numberOfGuests: 0,
+    customerId: "",
+  });
+
+  useEffect(() => {
+    axios
+      .get<IBooking[]>(
+        "https://school-restaurant-api.azurewebsites.net/booking/restaurant/65cdf38894d2af1c6aeae91d"
+      )
+      .then((response) => {
+        setBookings([...response.data]);
+      });
+  }, []);
+
   const handleSwitch = () => {
     setShowLogIn(true);
     setShowButton(false);
@@ -21,6 +56,45 @@ export const Admin = () => {
       setShowLogIn(false);
     }
   };
+  const handleBookingChange = () => {
+    setShowChange(true);
+    setShowAdmin(false);
+  };
+
+  const sendChangedData = async (bookingID: string) => {
+    setUpdatedBooking({
+      ...updatedBooking,
+      _id: bookingToUpdate._id,
+      customerId: bookingToUpdate.customerId,
+    });
+
+    let updatedBookingData = {
+      id: bookingToUpdate,
+      restaurantId: restaurantID,
+      date: updatedBooking.date,
+      time: updatedBooking.time,
+      numberOfGuests: updatedBooking.numberOfGuests,
+      customerId: bookingToUpdate.customerId,
+    };
+
+    const response = await axios.put(
+      "https://school-restaurant-api.azurewebsites.net/booking/update/" +
+        bookingID,
+      updatedBookingData
+    );
+  };
+  const handleRemoveBooking = async (bookingID: string) => {
+    const response = await axios.delete(
+      "https://school-restaurant-api.azurewebsites.net/booking/delete/" +
+        bookingID
+    );
+    setBookings((prevBookings) =>
+      prevBookings.filter((Allbooking) => Allbooking._id !== bookingID)
+    );
+
+    setShowBookingDone(true);
+  };
+
   return (
     <>
       <div className="body">
@@ -70,7 +144,58 @@ export const Admin = () => {
         )}
         {showAdmin && (
           <div className="container">
-            <div className="admin-page"> Hello Admin</div>
+            <div className="admin-page">
+              {bookings.map((allBookings) => {
+                return (
+                  <div key={allBookings._id} className="li-div">
+                    <li key={allBookings._id}>
+                      Booking ID:{allBookings._id} CustomerID:
+                      {allBookings.customerId} Date: {allBookings.date} Time:
+                      {allBookings.time} Guests:{allBookings.numberOfGuests}
+                    </li>
+
+                    <button
+                      onClick={() => handleRemoveBooking(allBookings._id)}
+                    >
+                      Remove
+                    </button>
+                    <button onClick={handleBookingChange}>Change info</button>
+                  </div>
+                );
+              })}
+            </div>
+          </div>
+        )}
+        {showChange && (
+          <div>
+            <p>Choose another date:</p>
+            <input
+              type="date"
+              value={updatedBooking.date}
+              onChange={(e) =>
+                setUpdatedBooking({ ...updatedBooking, date: e.target.value })
+              }
+            />
+            Choose different time:
+            <input
+              type="text"
+              value={updatedBooking.time}
+              onChange={(e) =>
+                setUpdatedBooking({ ...updatedBooking, time: e.target.value })
+              }
+            />
+            Choose amount of guests:
+            <input
+              type="number"
+              value={updatedBooking.numberOfGuests}
+              onChange={(e) =>
+                setUpdatedBooking({
+                  ...updatedBooking,
+                  numberOfGuests: parseInt(e.target.value),
+                })
+              }
+            />
+            <button>Update Booking</button>
           </div>
         )}
       </div>
